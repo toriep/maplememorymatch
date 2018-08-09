2a761e0d882622dee69a6d9842acee46fe3cc0f1
Torie Pham <tientp@uci.edu>
2018-08-08 20:59:44 -0700

    Memory Match version0.5 - Torie Pham

--- main.js
+++ main.js
@@ -1,26 +1,74 @@
 $(document).ready(initializeApp);
-var firstCardClicked = null;
-var secondCardClicked = null;
 
 function initializeApp(){
-    attachEventListener();
+    $('.card').click(card_clicked);
 }
 
-function attachEventListener(){
-    $(".card").click(handleCardClickEvent);
-}
+var first_card_clicked = null;
+var second_card_clicked=null;
+var total_possible_matches = 2;
+var match_counter= 0;
 
-function handleCardClickEvent(){
-    firstCardClicked = event.currentTarget;
-    if(firstCardClicked===null){
-        $(event.currentTarget).addClass('hidden');
+
+function card_clicked(){
+    $(event.currentTarget).find('.back').addClass('hidden');
+    if(first_card_clicked===null){
+        first_card_clicked=$(event.currentTarget);
+        // return first_card_clicked??
     } else {
-        secondCardClicked = event.currentTarget;
-    }
-    if('compare first with second using the image source of the front image if first clicked === second clicked'){
-        console.log('they match or they don\'t match');
+        if(first_card_clicked[0] === event.currentTarget){
+            return;
+        }
+        second_card_clicked=$(event.currentTarget);
+
+        if(first_card_clicked.find('.front img').attr('src')===second_card_clicked.find('.front img').attr('src')){
+            match_counter += 1;
+            first_card_clicked = null;
+            second_card_clicked = null;
+
+            if(match_counter===total_possible_matches){
+                console.log('YOU WON!!!');
+            }
+
+        } else if(first_card_clicked.find('.front img').attr('src')!==second_card_clicked.find('.front img').attr('src')){
+            $('.card').off();
+            setTimeout(function(){
+                $(first_card_clicked).find(".back").removeClass('hidden');
+                $(second_card_clicked).find(".back").removeClass('hidden');
+                first_card_clicked=null;
+                second_card_clicked=null;
+                initializeApp();
+            }, 800);
+        }
     }
 }
+
+
+
+
+// $(document).ready(initializeApp);
+// var firstCardClicked = null;
+// var secondCardClicked = null;
+//
+// function initializeApp(){
+//     attachEventListener();
+// }
+//
+// function attachEventListener(){
+//     $(".card").click(handleCardClickEvent);
+// }
+//
+// function handleCardClickEvent(){
+//     firstCardClicked = event.currentTarget;
+//     if(firstCardClicked===null){
+//         $(event.currentTarget).addClass('hidden');
+//     } else {
+//         secondCardClicked = event.currentTarget;
+//     }
+//     if('compare first with second using the image source of the front image if first clicked === second clicked'){
+//         console.log('they match or they don\'t match');
+//     }
+// }
 
 
 
