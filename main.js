0b21e6276dd7b9ffcf75326ee9d4d72daaab8b35
Torie Pham <tientp@uci.edu>
2018-08-10 19:11:58 -0700

    changed css

--- main.js
+++ main.js
@@ -2,73 +2,84 @@ $(document).ready(initializeApp);
 
 function initializeApp(){
     $('.card').click(card_clicked);
-}
+    var reset = $('button.reset');
+    reset.click(reset_stats);
+        }
 
 var first_card_clicked = null;
 var second_card_clicked=null;
-var total_possible_matches = 2;
+var total_possible_matches = 9;
 var match_counter= 0;
+var matches = 0;//increment by 1 every time the application finds a match
+var attempts = 0;//incremebt by 1 every time user clicks the 2nd card
+var accuracy = "";//matches/attempts
+var games_played = 0;//increment by 1 when the reset button is clicked
 
 
 function card_clicked(){
+    if($(event.currentTarget).hasClass('match')){
+        return;
+    }
     $(event.currentTarget).find('.back').addClass('hidden');
     if(first_card_clicked===null){
         first_card_clicked=$(event.currentTarget);
-        // return first_card_clicked??
-    } else {
-        if(first_card_clicked[0] === event.currentTarget){
+         } else {
+            attempts+=1;
+            if(first_card_clicked[0] === event.currentTarget){
             return;
         }
         second_card_clicked=$(event.currentTarget);
 
         if(first_card_clicked.find('.front img').attr('src')===second_card_clicked.find('.front img').attr('src')){
+            console.log('it\'s a match!');
+            first_card_clicked.addClass('match');
+            second_card_clicked.addClass('match');
+            first_card_clicked.fadeOut(1000);
+            second_card_clicked.fadeOut(1000);
             match_counter += 1;
+            matches++;
+            // attempts+=1;
             first_card_clicked = null;
             second_card_clicked = null;
-
-            if(match_counter===total_possible_matches){
+                        if(match_counter===total_possible_matches){
                 console.log('YOU WON!!!');
             }
 
-        } else if(first_card_clicked.find('.front img').attr('src')!==second_card_clicked.find('.front img').attr('src')){
+        } else{
+            console.log('2nd click. It\'s a mismatch!');
+
             $('.card').off();
             setTimeout(function(){
                 $(first_card_clicked).find(".back").removeClass('hidden');
                 $(second_card_clicked).find(".back").removeClass('hidden');
                 first_card_clicked=null;
                 second_card_clicked=null;
-                initializeApp();
-            }, 800);
+                $('.card').click(card_clicked);
+                $('.card').on();
+            }, 400);
         }
     }
+    accuracy = matches/attempts;
+    accuracy = Math.round(accuracy*100) + "%";
+    display_stats();
 }
 
+function display_stats(){
+    $('.games-played > .value').text(games_played);
+    $('.attempts > .value').text(attempts);
+    $('.accuracy > .value').text(accuracy);
+    $('.matches > .value').text(matches);
+}
 
+function reset_stats(){
+    console.log('Reset button clicked');
+    accuracy = 0;//good
+    matches = 0;//good
+    attempts = 0;//good
+    games_played+=1;//good
+    display_stats();
 
-
-// $(document).ready(initializeApp);
-// var firstCardClicked = null;
-// var secondCardClicked = null;
-//
-// function initializeApp(){
-//     attachEventListener();
-// }
-//
-// function attachEventListener(){
-//     $(".card").click(handleCardClickEvent);
-// }
-//
-// function handleCardClickEvent(){
-//     firstCardClicked = event.currentTarget;
-//     if(firstCardClicked===null){
-//         $(event.currentTarget).addClass('hidden');
-//     } else {
-//         secondCardClicked = event.currentTarget;
-//     }
-//     if('compare first with second using the image source of the front image if first clicked === second clicked'){
-//         console.log('they match or they don\'t match');
-//     }
-// }
-
+    $('.back').removeClass('hidden');//good
+}
 
 
