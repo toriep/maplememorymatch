e1a0abd465405a42fab97eab5b7cacd2675ad8e7
Torie Pham <tientp@uci.edu>
2018-09-13 12:39:43 -0700

    fixed background attachment css

--- main.js
+++ main.js
@@ -45,10 +45,10 @@ function pauseBackgroundMusic() {
     backgroundMusic.pause();
 };
 
-var first_card_clicked = null;
+var first_card_clicked = null;//if this is null, it's the first card to be compared later
 var second_card_clicked = null;
 var total_possible_matches = 9;
-var match_counter = 0;
+var match_counter = 0;//when this reaches 9, the user wins the game
 var matches = 0; //increment by 1 every time the application finds a match
 var attempts = 0; //incremebt by 1 every time user clicks the 2nd card
 var accuracy = 0; //matches/attempts
@@ -100,7 +100,7 @@ function displayCards(array) {
 };
 
 function card_clicked() {
-    var clickSound = new Audio('click.mp3');
+    var clickSound = new Audio('sound.wav');
     //if card clicked has class of match, function doesn't run further
     if ($(event.currentTarget).hasClass('match')) {
         return;
@@ -110,10 +110,10 @@ function card_clicked() {
     if (first_card_clicked === null) { //first card clicked
         first_card_clicked = $(event.currentTarget);
         clickSound.play();
-    } else { //2nd card clicked
+    } else { //if first_card_clicked is not null, this is a the second card
         attempts += 1;
-        if (first_card_clicked[0] === event.currentTarget) { //what does this line mean exactly?
-            return;
+        if (first_card_clicked[0] === event.currentTarget) {//if user clicks on a front card twice
+            return;//disable further card comparison
         }
         second_card_clicked = $(event.currentTarget);
         //If second card's image source is the same as first's card image source
@@ -128,7 +128,7 @@ function card_clicked() {
             match_counter += 1;
             matches++;
             accuracy = matches / attempts;
-            first_card_clicked = null;
+            first_card_clicked = null;//reset first_card_clicked for future match comparisoin
             second_card_clicked = null;
             if (match_counter === total_possible_matches) {
                 console.log('YOU WON!!!');
@@ -152,7 +152,7 @@ function card_clicked() {
                 $(second_card_clicked).find(".back").removeClass('hidden');
                 first_card_clicked = null;
                 second_card_clicked = null;
-                $('.card').click(card_clicked); //What does this line do?
+                $('.card').click(card_clicked); //re-attaches clickhandler to .card
                 $('.card').on();
             }, 700);
         }
@@ -176,9 +176,6 @@ function reset_stats() {
     attempts = 0; //good
     games_played += 1; //good
     display_stats();
-    // $('.back').removeClass('hidden'); //show the back cards again
-    // $('.card').fadeIn(100); //makes the faded cards reappear
-    // $('.card').removeClass('match'); //so that when you reset, you can flip cards that were matched from previous game
     $('#game-area').empty();
     displayCards(cardImages);
     $('.card').click(card_clicked);
